77e29c892eaa96428e3d1d37e37ee4b4481ab4b3
JanStastny2 <stastnyj162@gmail.com>
2025-05-26 20:45:40 +0200

    bug weightrecordserviceIMPL idk proc

--- GrainWeightApp/src/main/java/cz/uhk/grainweight/repository/WeightRecordRepository.java
+++ GrainWeightApp/src/main/java/cz/uhk/grainweight/repository/WeightRecordRepository.java
@@ -2,6 +2,7 @@ package cz.uhk.grainweight.repository;
 
 import cz.uhk.grainweight.model.WeightRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface WeightRecordRepository extends JpaRepository<WeightRecord, Long> {
 }
--- GrainWeightApp/src/main/java/cz/uhk/grainweight/service/WeighingRecordService.java
+++ GrainWeightApp/src/main/java/cz/uhk/grainweight/service/WeighingRecordService.java
@@ -4,6 +4,16 @@ import cz.uhk.grainweight.model.WeightRecord;
 import java.util.List;
 
 public interface WeighingRecordService {
-    WeightRecord findById(Long id);
+    List<WeightRecord> getAllWeighingRecords();
+    WeightRecord getWeighingRecordById(Long id);
+    WeightRecord saveWeighingRecord(WeightRecord weighingRecord);
+    void deleteWeightRecord(long id);
 
+    List<WeightRecord> getAllWeightRecords();
+
+    WeightRecord getWeightRecord(Long id);
+
+    WeightRecord saveWeightRecord(WeightRecord weightRecord);
+
+    void deleteWeightRecord(Long id);
 }
