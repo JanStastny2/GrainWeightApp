1d924c8cb16e4ed7276d3d2f846338500735f973
janstastny <jan.stastny@akcenta.cz>
2026-04-03 10:49:51 +0200

    refactor

--- src/main/java/cz/uhk/grainweight/service/DriverService.java
+++ src/main/java/cz/uhk/grainweight/service/DriverService.java
@@ -1,12 +1,10 @@
 package cz.uhk.grainweight.service;
 
 import cz.uhk.grainweight.model.Driver;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public interface DriverService {
     List<Driver> getAllDrivers();
     void saveDriver(Driver driver);
